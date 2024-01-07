c78167d487079028f02766b996ca6ab11017ac5b
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-07 16:31:05 +0400

    Register form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,10 @@
 import { LoginForm }from "@/components/auth/login-form"
 
 
-const page = () => {
+const LoginPage = () => {
     return (
         <LoginForm />
     )
 }
 
-export default page
+export default LoginPage;
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -5,34 +5,35 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import * as z from "zod";
-import { LoginSchema } from '@/schemas';
+import { RegisterSchema } from '@/schemas';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-success';
-import { login } from '@/actions/login';
+import { register } from '@/actions/register';
 import { useState, useTransition } from 'react';
 
 
-export const LoginForm= () => {
+export const RegisterForm= () => {
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
 
-    const form  = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form  = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
-            password: ""
+            password: "",
+            name: ""
         },
     });
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(() => {
-            login(values)
+            register(values)
             .then((data) => {
                 setError(data.error);
                 setSuccess(data.success);
@@ -42,9 +43,9 @@ export const LoginForm= () => {
 
     return (
         <CardWrapper
-            headerLabel="Welcome Back"
-            backButtonLabel="Don't have an account ?"
-            backButtonHref="/auth/register"
+            headerLabel="Create an account"
+            backButtonLabel="Already have an account ?"
+            backButtonHref="/auth/login"
             showSocial
         >
             <Form {...form}>
@@ -53,6 +54,25 @@ export const LoginForm= () => {
                     className='space-y-6'
                 >
                     <div className='space-y-4'>
+                        <FormField 
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                            {...field}
+                                            placeholder='Exemple'
+                                            disabled={isPending}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
                         <FormField 
                             control={form.control}
                             name="email"
@@ -97,7 +117,7 @@ export const LoginForm= () => {
                     <FormError message={error} />
                     <FormSuccess message={success} />
                     <Button type='submit' className='w-full' disabled={isPending}>
-                        Login
+                        Create an account
                     </Button>
                 </form>
             </Form>
