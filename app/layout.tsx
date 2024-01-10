5c34bee4aa9a96ec6a5ce195bd553bb8cae8afc3
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-10 17:38:28 +0400

    Admin Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { SessionProvider } from 'next-auth/react'
 import './globals.css'
 import { auth } from '@/auth'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <SessionProvider session={session}>
             <html lang="en">
                 <body className={inter.className}>
+                    <Toaster />
                     {children}
                 </body>
             </html>
